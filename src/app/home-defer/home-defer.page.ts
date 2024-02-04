4945c81f60e47478af5ac61f015ddd9a7899d671
Assaf <1214assaf@gmail.com>
2024-02-04 17:40:52 +0200

    Update home-defer page with trending movies and loading functionality playing with defer blocks features

--- src/app/home-defer/home-defer.page.ts
+++ src/app/home-defer/home-defer.page.ts
@@ -1,20 +1,70 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule, DatePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { IonHeader, IonLabel , IonToolbar, IonTitle, IonContent, IonInfiniteScroll,InfiniteScrollCustomEvent, IonAlert, IonList, IonAvatar, IonItem, IonSkeletonText, IonBadge, IonInfiniteScrollContent } from '@ionic/angular/standalone';
+import { RouterModule } from '@angular/router';
+import { MovieService } from '../services/movie.service';
+import { MovieResult } from '../services/interfaces';
+import { catchError, finalize } from 'rxjs';
 
 @Component({
   selector: 'app-home-defer',
   templateUrl: './home-defer.page.html',
   styleUrls: ['./home-defer.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonAvatar, IonSkeletonText
+    ,IonAlert, IonLabel, DatePipe, RouterModule, IonBadge, IonInfiniteScroll, IonInfiniteScrollContent],
 })
-export class HomeDeferPage implements OnInit {
+export class HomeDeferPage{
+  private moviesService = inject(MovieService);
+  private currentPage = 1;
+  public error = null;
+  public movies: MovieResult[] = [];
+  public isLoading = false;
+  public imageBaseUrl = 'https://image.tmdb.org/t/p';
+  public dummyArray = new Array(5);
 
-  constructor() { }
+  constructor() {
+    this.loadMovies();
+  }
 
-  ngOnInit() {
+  loadMovies(event? : InfiniteScrollCustomEvent){ {
+    this.error = null;
+
+    if (!event){
+      this.isLoading = true;
+    }
+
+    this.moviesService.getTopRatedMovies(this.currentPage).pipe(
+      finalize(() => {
+        this.isLoading = false;
+        if (event){
+          event.target.complete();
+        }
+      }),
+      catchError((error: any) => {
+        console.log(this.error);
+        this.error = error.status_message;
+        return [];
+      })
+    ).subscribe({
+      next: (data) => {
+        console.log(data);
+        this.movies = [...this.movies, ...data.results];
+        this.currentPage++;
+        if (event){
+          event.target.disabled = data.total_pages === this.currentPage;
+        }
+      }
+    });
+  };
+
+
+  }
+    loadMore(event?: InfiniteScrollCustomEvent){
+      this.currentPage++;
+      this.loadMovies(event);
   }
 
 }
--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -19,6 +19,7 @@ export class MovieService {
   getTopRatedMovies(page = 1): Observable<ApiResult> {
     return this.http.get<ApiResult>(`${BASE_URL}/movie/top_rated?page=${page}&api_key=${API_KEY}`)
     .pipe(
+      delay(5000)
     );
   }
 
